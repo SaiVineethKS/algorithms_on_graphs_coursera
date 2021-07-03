688c71666c0fc6471006ebd232741dcee6fc3e9d
SaiVineethKS <kssaivineeth@gmail.com>
2021-07-04 01:28:37 +0530

    toposort

--- week2/week2_graph_decomposition2/2_order_of_courses/toposort.py
+++ week2/week2_graph_decomposition2/2_order_of_courses/toposort.py
@@ -3,15 +3,43 @@
 import sys
 
 def dfs(adj, used, order, x):
-    #write your code here
-    pass
+    """
+    The key idea here is to pick a source node
+    keep DFSing till it reaches a sink node
+    
+    Due to the lack of adjacent nodes, sink node will 
+    return its function call. Followed by a series of 
+    'returns' from all functional calls in the stack
+    until the initial source node's call.
 
+    Here, append the initial source node to graph
+    """
+    if used[x] == 1:
+        # If x is already used in the toposort
+        return
+    
+    # Keep tracking if the current 
+    # variable is already during the DFS process
+    used[x] = 1
+    
+    for e in adj[x]:
+        # Keep DFSing till you find a sink
+        dfs(adj, used, order, e)
+    
+    # Once a sink is found add the initial node
+    # the recursion call stack
+    order.append(x)
 
 def toposort(adj):
     used = [0] * len(adj)
-    order = []
-    #write your code here
-    return order
+    postvisit_order = []
+    
+    for v in range(len(adj)):
+        dfs(adj, used, postvisit_order, v)
+
+    # Actual topological sort is the reverse
+    # of post-visit order
+    return reversed(postvisit_order)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
