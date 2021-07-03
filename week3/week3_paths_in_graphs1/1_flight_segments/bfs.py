c60b21209d719a702d72efb18866d3ba8368df81
SaiVineethKS <kssaivineeth@gmail.com>
2021-07-04 05:23:51 +0530

    BFS to calculate distance

--- week3/week3_paths_in_graphs1/1_flight_segments/bfs.py
+++ week3/week3_paths_in_graphs1/1_flight_segments/bfs.py
@@ -2,14 +2,29 @@
 
 import sys
 import queue
+import math
 
-def distance(adj, s, t):
+def distance(adj, start, end):
     #write your code here
+    queue,visited = [], []
+    queue.append(start)
+    visited.append(start)
+    distance = [math.inf] * len(adj)
+    distance[start] = 0
+    while len(queue) > 0:
+        node = queue.pop(0)
+        if node == end:
+            return distance[node]
+        visited.append(node)
+        for next_node in [n for n in adj[node] if n not in visited]:
+            distance[next_node] = distance[node] + 1
+            queue.append(next_node)
     return -1
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    # input = sys.stdin.read()
+    # data = list(map(int, input.split()))
+    data = [4,4,1,2,4,1,2,3,3,1,2,4]
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
